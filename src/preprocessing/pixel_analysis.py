970330898a9dd20bb7264f191bfa0c019ec82ce8
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2022-10-11 12:38:42 +0100

    Update tiling pipeline and add pixel analysis
    - Modified existing tiling pipeline to discard blank tiles and save
      space
    - Include pixel level analysis to identify pixels belonging to blank,
      partial and good tiles.
    - Filter tiles based on pixel level data

---
+++ src/preprocessing/pixel_analysis.py
@@ -0,0 +1,32 @@
+import tifffile as tiff
+import numpy as np
+import glob
+import sys
+
+
+#Let us define a function to detect blank tiles and tiles with very minimal information
+#This function can be used to identify these tiles so we can make a decision on what to do with them. 
+#Here, the function calculates mean and std dev of pixel values in a tile. 
+def find_mean_std_pixel_value(img_list):
+    avg_pixel_value =[]
+    stddev_pixel_value = []
+    for file in img_list:
+        image = tiff.imread(file)
+        avg = image.mean()
+        std = image.std()
+        avg_pixel_value.append(avg)
+        stddev_pixel_value.append(std)
+        
+    avg_pixel_value = np.array(avg_pixel_value)
+    stddev_pixel_value = np.array(stddev_pixel_value)
+    
+    print("Average pixel value for images is: ", avg_pixel_value.mean())
+    print("Average std dev of pixel values for images is: ", stddev_pixel_value.mean())
+    
+    return (avg_pixel_value, stddev_pixel_value)
+
+if __name__ == '__main__':
+    orig_tile_dir = sys.argv[1]
+    img_list = glob.glob(orig_tile_dir + "*.tif")
+    
+    img_stats = find_mean_std_pixel_value(img_list)
