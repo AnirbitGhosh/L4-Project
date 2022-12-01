fe5d34c0498c5059226a454d5484c47a94c37e90
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2022-12-01 03:39:24 +0000

    fix tile_slide to only save tiles with size 96x96x3

--- src/preprocessing/tile-slide.py
+++ src/preprocessing/tile-slide.py
@@ -8,18 +8,37 @@ import os
 from pixel_analysis import find_mean_std_pixel_value
 from openslide.deepzoom import DeepZoomGenerator
 import tifffile as tiff
+import argparse
 
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+    
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', type=dir_path, help='Pass path to dir containing whole slide images with -i or --input flags' )
+parser.add_argument('-o', '--output', type=dir_path, help="Pass directoryto output tiles directoy using -o or --output flags")
+parser.add_argument('-s', '--size',default=96, type=int, help="Pass dimension of the tile (DEFAULT=96) -s or --size flags")
 
 
 if __name__ == '__main__':
-    for f in os.listdir(sys.argv[1]):
-        file_path = os.path.join(sys.argv[1], f)
+    if len(sys.argv) < 4:
+        parser.print_usage()
+        parser.print_help()
+        sys.exit(1)
+    
+    print("Parsing arguments...")
+    args = parser.parse_args()
+    print("Parsing arguments... DONE")
+
+    for f in os.listdir(args.input)[1:2]:
+        file_path = os.path.join(args.input, f)
         slide = open_slide(file_path)
         # Load the slide file (svs) into an object
         slide_props = slide.properties
         print(slide_props)
 
-        print("Vendor is:", slide_props["openslide.vendor"])
         print("Pixel size of X in um is:", slide_props["openslide.mpp-x"])
         print("Pixel size of Y in um is:", slide_props['openslide.mpp-y'])
 
@@ -31,16 +50,6 @@ if __name__ == '__main__':
         slide_dims = slide.dimensions
         print("Dimensions of the slide at level 0 - max res: ", slide_dims)
 
-        # Get a thumbnail of the image and visualize
-        slide_thumb_600 = slide.get_thumbnail(size=(600,600))
-        # slide_thumb_600.show()
-
-        # convert thumbnail to np array
-        slide_thumb_600_np = np.array(slide_thumb_600)
-        plt.figure(figsize=(8,8))
-        plt.imshow(slide_thumb_600_np)
-        # plt.show()
-
         # get slide dims at each level. Remeber that a whole slide image stores information as pyramid at various levels
         dims = slide.level_dimensions
         num_level = len(dims)
@@ -51,21 +60,11 @@ if __name__ == '__main__':
         factors = slide.level_downsamples
         print("each level is downsampled by an amount of:", factors)
 
-        # Copy an image from a level 
-        level3_dim = dims[2]
-        # Give pixel coordinates (top left pixel in the original large image)
-        # Also give the level number (for level 3 we are providing a valueof 2)
-        # Remember that the output would be a RGBA image (NOT RGB)
-        level3_img = slide.read_region((0,0), 2, level3_dim) # Pillow object, mode=RGBA
-
-        # convert the iamge to RGB
-        level3_img_RGB = level3_img.convert('RGB')
-        #level3_img_RGB.show()
 
         #################################
 
         # Generate object for tiles using the DeepZoomGenerator
-        tiles = DeepZoomGenerator(slide, tile_size=int(sys.argv[3]), overlap=0, limit_bounds=False)
+        tiles = DeepZoomGenerator(slide, tile_size=int(args.size), overlap=0, limit_bounds=False)
         #Here, we have divided our SVS file into tiles of size 244x244 with no overlap
 
         #The tiles object also contains data at many levels. 
@@ -86,7 +85,7 @@ if __name__ == '__main__':
         cols, rows = tiles.level_tiles[level_num] # for whole slide's tiles
         #cols, rows = 100, 100
 
-        tile_dir = sys.argv[2]
+        tile_dir = args.output
         sub_dir = os.path.basename(file_path)
         sub_dir_path = os.path.join(tile_dir, sub_dir)
         sub_dir_path = os.path.splitext(sub_dir_path)[0]
@@ -99,12 +98,12 @@ if __name__ == '__main__':
                 temp_tile = tiles.get_tile(level_num, (col, row))
                 temp_tile_RGB = temp_tile.convert('RGB')
                 temp_tile_RGB_np = np.array(temp_tile_RGB)
-
+                
                 # only save tiles with >50% tissue samples
                 # remove blank and partial tiles            
-                if temp_tile_RGB_np.std() > 15 and temp_tile_RGB_np.mean() < 230: 
-                     print("saving tile number:", tile_name)
-                     tiff.imsave(sub_dir_path + "/" +tile_name + ".tif", temp_tile_RGB_np)
+                if temp_tile_RGB_np.std() > 15 and temp_tile_RGB_np.mean() < 230 and temp_tile_RGB_np.shape == (96,96,3): 
+                    print("saving tile number:", tile_name)
+                    tiff.imsave(sub_dir_path + "/" +tile_name + ".tif", temp_tile_RGB_np)
     
 
 
